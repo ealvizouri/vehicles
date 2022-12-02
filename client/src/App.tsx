ef186e47590b20956b50af08bf7b22ccb5ac9746
Mariano Alvizouri <ealvizouri@gmail.com>
2022-12-02 16:37:32 -0600

    code cleaning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-/* import { Counter } from './features/counter/Counter'; */
 import { RouterProvider } from 'react-router-dom';
 import router from './app/router';
 import './App.css';
@@ -6,10 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <Counter />
-        
-      </header> */}
       <RouterProvider router={router} />
     </div>
   );
